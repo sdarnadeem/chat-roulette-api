1d743fb4da49492600d9f48863c098129c757742
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-06-07 16:30:21 +0530

    fix simple-peer error

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 
-const users = [];
+const users = new Set();
 
 const io = new Server(httpServer, {
   cors: {
@@ -15,6 +15,27 @@ const io = new Server(httpServer, {
   },
 });
 
+const findSomeone = () => {
+  console.log(users);
+  let newUsers = Array.from(users);
+  if (newUsers.length < 2) {
+    return null;
+  }
+  const index1 = Math.floor(Math.random() * newUsers.length);
+  const user1 = newUsers[index1];
+
+  console.log("user1", user1, index1);
+
+  newUsers = newUsers.filter((user, i) => i !== index1);
+  const index2 = Math.floor(Math.random() * newUsers.length);
+  const user2 = newUsers[index2];
+  console.log("user1", user1, index1);
+
+  const connectedUsers = [user1, user2];
+
+  return connectedUsers;
+};
+
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
 
@@ -22,19 +43,36 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("callEnded");
   });
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
-    });
+  socket.on("find-someone", (userId) => {
+    users.add(userId);
+    const connectedUsers = findSomeone();
+
+    if (connectedUsers === null) {
+      // io.to(connectedUsers[0]).emit("error", "no user found");
+      // io.to(connectedUsers[1]).emit("error", "no user found");
+      console.log("2 users not connected");
+      return;
+    }
+    console.log(connectedUsers.length);
+
+    console.log("connected users", connectedUsers);
+    io.to(connectedUsers[0]).emit("make-call", connectedUsers[1]);
+    io.to(connectedUsers[1]).emit("answer-call", connectedUsers[0]);
+    users.delete(connectedUsers[0]);
+    users.delete(connectedUsers[1]);
   });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted"), data.signal;
-  });
+  //   socket.on("callUser", (data) => {
+  //     io.to(data.userToCall).emit("callUser", {
+  //       signal: data.signalData,
+  //       from: data.from,
+  //       name: data.name,
+  //     });
+  //   });
+
+  //   socket.on("answerCall", (data) => {
+  //     io.to(data.to).emit("callAccepted"), data.signal;
+  //   });
 });
-
-console.log(users);
 
 httpServer.listen(3000);
